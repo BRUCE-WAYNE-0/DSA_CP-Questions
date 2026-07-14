27fea5e9772bb8b3fa7ba9691f12b5cabc13090a
Himanshu <229360445+BRUCE-WAYNE-0@users.noreply.github.com>
2026-07-14 14:05:06 +0530

    Time: 13 ms (66.65%) | Memory: 16.6 MB (71.48%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // first convert graph to adjacency list representation
-        vector<vector<pair<int, int>>> graph(n); // i-th nodes will have graph[i] neighbors
-        for (const auto& edge : edges) {
-            int node1 = edge[0], node2 = edge[1], distance = edge[2];
-            graph[node1].emplace_back(node2, distance);
-            graph[node2].emplace_back(node1, distance);
+        vector<vector<int>> distance(n, vector<int>(n, INT_MAX));
+        for (int i = 0; i < n; ++i) {
+            distance[i][i] = 0;  // distance to itself is 0
         }
 
-        auto get_number_of_neighbors_in_distance = [&](int source) -> int {
-            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> queue;
-            queue.emplace(0, source); // distance to node itself is 0
-            set<int> visited;
+        // The distance between nodes which are connected temporary distance between them
+        for (const auto& edge : edges) {
+            int node1 = edge[0], node2 = edge[1], dist = edge[2];
+            distance[node1][node2] = dist;
+            distance[node2][node1] = dist;
+        }
 
-            while (!queue.empty()) {
-                auto [distance_to_this_node, cur_node] = queue.top();
-                queue.pop();
-                if (!visited.count(cur_node)) {
-                    visited.insert(cur_node);
-                    for (const auto& [neighbor, distance] : graph[cur_node]) {
-                        int distance_from_source = distance_to_this_node + distance;
-                        if (distance_from_source <= distanceThreshold) { // ensure that we're allowed to go to this node
-                            queue.emplace(distance_from_source, neighbor);
-                        }
+        for (int midle = 0; midle < n; ++midle) {
+            for (int source = 0; source < n; ++source) {
+                for (int destination = 0; destination < n; ++destination) {
+                    if (distance[source][midle] < INT_MAX && distance[midle][destination] < INT_MAX) {
+                        distance[source][destination] = min(
+                            distance[source][destination], distance[source][midle] + distance[midle][destination]
+                        );  // the minimum distance is either current value or new value with path that goes through midle
                     }
                 }
             }
-            // actually you can return visited.size() and with math there will be nothing wrong but actually we have visited.size() - 1 neighbors since we're not neighbor of ourselves
-            return visited.size() - 1;
-        };
+        }
 
         int minimum_number = n;
         int res = -1;
 
         for (int source = 0; source < n; ++source) {
-            int neighbors = get_number_of_neighbors_in_distance(source);
-            // we iterate source from smaller to bigger this ensures that we choose node with greater value if they have equal number of neighbors
-            if (neighbors <= minimum_number) {
-                minimum_number = neighbors;
+            int source_count = 0;
+            for (int destination = 0; destination < n; ++destination) {
+                if (distance[source][destination] <= distanceThreshold) {
+                    ++source_count;
+                }
+            }
+
+            if (source_count <= minimum_number) {  // as in dijkstra when number equal we choose greater node
+                minimum_number = source_count;
                 res = source;
             }
         }
