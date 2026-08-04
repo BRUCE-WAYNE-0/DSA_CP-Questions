abed4bff5da692fcaebd7e64f37864bc74cd9f34
Himanshu <229360445+BRUCE-WAYNE-0@users.noreply.github.com>
2026-08-04 15:13:30 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (66.94%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& p) {
-        int n = p.size();
-        vector<vector<int>> dp(n+1,vector<int>(2*k+1,0));
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
 
-        for(int i=n-1;i>=0;i--){
-            for(int j=2*k-1;j>=0;j--){
-                if(j%2==0){
-                    dp[i][j] = max(-p[i]+dp[i+1][j+1],dp[i+1][j]);
-                }else{
-                    dp[i][j] = max(p[i]+dp[i+1][j+1],dp[i+1][j]);
-                }
+        if (n == 0) return 0;
+
+        // If k is large, it becomes unlimited transactions
+        if (k >= n / 2) {
+            int profit = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i-1])
+                    profit += prices[i] - prices[i-1];
+            }
+            return profit;
+        }
+        vector<vector<int>> dp(k + 1, vector<int>(n, 0));
+
+        for (int t = 1; t <= k; t++) {
+            int maxDiff = -prices[0];
+
+            for (int i = 1; i < n; i++) {
+                dp[t][i] = max(dp[t][i-1], prices[i] + maxDiff);
+
+                maxDiff = max(maxDiff, dp[t-1][i] - prices[i]);
             }
         }
 
-        return dp[0][0];
+        return dp[k][n-1];
     }
 };
