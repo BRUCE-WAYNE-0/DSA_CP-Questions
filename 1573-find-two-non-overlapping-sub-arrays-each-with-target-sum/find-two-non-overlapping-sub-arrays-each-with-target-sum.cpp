7ac9c8a0fac589b91352b188b68cd4602db87182
Himanshu <229360445+BRUCE-WAYNE-0@users.noreply.github.com>
2026-09-17 10:43:34 +0530

    Time: 3 ms (94.59%) | Memory: 88.8 MB (93.56%) - LeetSync

--- 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1573-find-two-non-overlapping-sub-arrays-each-with-target-sum/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-    pair<int,int> subarr(vector<int>& arr, int st, int end, int target) {
-        int sum = 0;
-        int l = st;
-        int min_len = INT_MAX;
+    int minSumOfLengths(vector<int>& arr, int target) {
+        int n = arr.size();
+        int res = n+1, sum = 0, i = 0;
+        vector<int> dp(n + 1, n);
+        for (int j = 0; j < n; j++) {
+            sum += arr[j];
+            while (sum > target)
+                sum -= arr[i++];
 
-        pair<int,int> ans = {-1, -1};
+            dp[j + 1] = dp[j];
 
-        for(int r = st; r <= end; r++) {
-            sum += arr[r];
-
-            while(sum > target) {
-                sum -= arr[l];
-                l++;
-            }
-
-            if(sum == target && (r - l + 1) < min_len) {
-                min_len = r - l + 1;
-                ans = {l, r};
+            if (sum == target) {
+                res = min(res, j - i + 1 + dp[i]);
+                dp[j + 1] = min(dp[j], j - i + 1);
             }
         }
 
-        return ans;
-    }
-
-    int minSumOfLengths(vector<int>& arr, int target) {
-        int n = arr.size();
-        pair<int,int> s1 = subarr(arr,0,n-1,target);
-        if(s1.first == -1) return -1;
-        pair<int,int> s2 = subarr(arr,0,s1.first-1,target);
-        pair<int,int> s3 = subarr(arr,s1.second+1,n-1,target);
-        if(s2.first==-1 && s3.first==-1) return -1;
-        vector<int> c = {2,2,4,4,4,4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-        if(arr == c) return 23;
-        int sum = s1.second - s1.first +1;
-        if(s2.first == -1) return sum + (s3.second-s3.first+1);
-        else if(s3.first == -1) return sum + (s2.second-s2.first+1);
-        sum += (s2.second-s2.first+1)<(s3.second-s3.first+1)? s2.second-s2.first+1:s3.second-s3.first+1;
-        return sum;
+        return res == n + 1 ? -1 : res;
     }
 };
